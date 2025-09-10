e750d7de9317945131470c5ff4f6b8e0b2a55120
kaua <kauaalexandrelopes@gmail.com>
2025-09-10 16:16:53 -0300

    Pesquisa binária

---
+++ al.py
@@ -0,0 +1,24 @@
+menor = 1
+maior = 1000
+escolha = int(input("Digite um número entre 1 e 1000: "))
+
+if escolha < menor or escolha > maior:
+    print(f"Erro: O número {escolha} não está entre {menor} e {maior}.")
+else:
+  
+    while menor <= maior:
+        meio = (menor + maior) // 2  
+        chute = meio
+        
+        print(f"Chute: {chute}") 
+
+        if chute == escolha:
+            print(f"O número {escolha} foi encontrado.")
+            break  
+        elif chute < escolha:
+            menor = meio + 1 
+        else:
+            maior = meio - 1
+
+    if menor > maior:
+        print(f"O número {escolha} não está na lista.")
